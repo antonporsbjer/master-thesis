2fe0015b9a7ab306337e99de5e6df2e50045e7ce
Julian Ley <146943186+JulianLey@users.noreply.github.com>
2024-06-04 16:17:51 +0200

    Popup Menu added
    Added a Menu that opens up when you press a button

---
+++ MetropedInteractive/Assets/UIScripts/PanelOpener.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+    void Start ()
+    {
+        Panel.SetActive(false);
+    }
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+
+}
