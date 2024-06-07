23e3d629d04ba958855fc2e069ed2cf6c670ff54
Julian Ley <146943186+JulianLey@users.noreply.github.com>
2024-06-07 16:49:08 +0200

    Rating can be saved
    Updated the code so that the rating can be saved under a hardcoded Example player ID

--- MetropedInteractive/Assets/UIScripts/GameManager.cs
+++ MetropedInteractive/Assets/UIScripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,11 +8,11 @@ public class GameManager : MonoBehaviour
     public bool IsMovementPaused { get; private set; }
 
     public GameDataManager gameDataManager;
+    public Slider ratingSlider; // Reference to the slider UI component
 
     void Start()
     {
         gameDataManager = GetComponent<GameDataManager>();
-
         gameDataManager.readFile();
     }
 
@@ -35,8 +36,9 @@ public class GameManager : MonoBehaviour
 
     public void SaveGame()
     {
-        GameData newGameData = new GameData(442387, 4);
+        int playerId = 442387; // Example player ID
+        int rating = (int)ratingSlider.value; // Get the rating from the slider
+        GameData newGameData = new GameData(playerId, rating);
         gameDataManager.SaveGameData(newGameData);
     }
 }
-
